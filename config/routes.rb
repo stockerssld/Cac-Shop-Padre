95c0c467d651ff15a9b01a7236b6760ceb22817a
José Antonio PAdre García <stockerssld@gmail.com>
2019-07-23 09:27:11 -0500

    Add User and routes registered an unregistered

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   get 'welcome/unregistered'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
