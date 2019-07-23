752d317b80f81863fa72679f82925d5e5f588a8c
José Antonio PAdre García <stockerssld@gmail.com>
2019-07-23 09:27:55 -0500

    Add routes register unregister

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  get 'welcome/unregistered'
+  # get 'welcome/index'
+  # get 'welcome/unregistered'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root to: "welcome#index"
+  end
+  unauthenticated :user do
+    devise_scope :user do 
+      root to: "welcome#unregistered"
+    end
+  end
 end
