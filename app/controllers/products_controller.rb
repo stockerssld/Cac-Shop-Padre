67772364ba8bd5257de4858baea117f1ba46da83
José Antonio PAdre García <stockerssld@gmail.com>
2019-08-02 17:33:25 -0500

    controlle view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
-  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :show]
   
   # before_action SetProduct
   
